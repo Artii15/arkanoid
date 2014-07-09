db1b21c7176ddf9307a6d02a14bc04845eeabf80
whitedrag <wojciechsikoraws@gmail.com>
2014-07-09 13:18:16 +0200

    Dodanie obsługi klawiszy i ruchu paletką

--- src/Bat.cpp
+++ src/Bat.cpp
@@ -1,3 +1,12 @@
 #include "../inc/Bat.h"
 
-Bat::~Bat(){}	
+Bat::Bat(){
+
+}
+
+Bat::~Bat(){
+}
+Bat& Bat::move(float x){
+	model_matrix = glm::translate(model_matrix, glm::vec3(x, 0.0f, 0.0f));
+	return *(this);
+}
